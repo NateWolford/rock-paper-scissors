acc7c737ec78bcd410a3bb1cd6670020d49ae3f0
Nate Wolford <natenwolford@gmail.com>
2023-09-11 20:51:09 -0700

    test logic

--- game.js
+++ game.js
@@ -13,8 +13,6 @@ function playRound(playerSelection, computerSelection) {
 
     playerSelection = firstLetter + otherLetters
 
-    computerSelection = getComputerChoice()
-
     if (playerSelection === computerSelection) {
         return "It's a draw."
     } else if (playerSelection === 'Rock' && computerSelection === 'Scissors' || 
@@ -32,21 +30,49 @@ function game() {
     let userScore = 0
     let computerScore = 0
 
-    
+    const rockButton = document.getElementById("Rock")
+    const paperButton = document.getElementById("Paper")
+    const scissorsButton = document.getElementById("Scissors")
 
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Rock, paper, or scissors?")
-        const computerSelection = getComputerChoice()
+ 
+
+    rockButton.addEventListener("click", function() {
+        let playerSelection = "rock"
+        let computerSelection = getComputerChoice()
         const result = playRound(playerSelection, computerSelection)
 
-        console.log(result)
-        
         if(result.includes("won")) {
             userScore++
         } else if(result.includes("lost")) {
             computerScore++
         }
-    }
+        document.getElementById("rounds").innerHTML = result
+    })
+
+    paperButton.addEventListener("click", function() {
+        let playerSelection = "paper"
+        let computerSelection = getComputerChoice()
+        const result = playRound(playerSelection, computerSelection)
+
+        if(result.includes("won")) {
+            userScore++
+        } else if(result.includes("lost")) {
+            computerScore++
+        }
+        
+    })
+
+    scissorsButton.addEventListener("click", function() {
+        let playerSelection = "scissors"
+        let computerSelection = getComputerChoice()
+        const result = playRound(playerSelection, computerSelection)
+
+        if(result.includes("won")) {
+            userScore++
+        } else if(result.includes("lost")) {
+            computerScore++
+        }
+    })
 
     if (userScore > computerScore) {
         console.log(`You won by a score of ${userScore} to ${computerScore}`)
@@ -55,7 +81,8 @@ function game() {
     } else if (computerScore === userScore) {
         console.log("It's a draw!")
     }
-}
+  }
+
 
 game()
  
